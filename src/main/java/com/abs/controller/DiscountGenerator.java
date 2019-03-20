dbaa194afc3b34e4142ef61760cbd37790d21d0d
Abdul Ghani Firoze <25895690+The-Elev8r@users.noreply.github.com>
2019-03-20 19:52:20 +0530

    initial commit: abs setup

---
+++ src/main/java/com/abs/controller/DiscountGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abs.controller;
+
+import com.abs.model.Product;
+
+/**
+ *
+ * @author ghani
+ */
+public class DiscountGenerator {
+
+    private void generateDiscount(Product product) {
+
+    }
+}
---
+++ src/main/java/com/abs/service/DiscountService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abs.service;
+
+import com.abs.model.Discount;
+
+/**
+ *
+ * @author ghani
+ */
+public interface DiscountService {
+        
+    public Discount getDiscount();
+}
